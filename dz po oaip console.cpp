f7245d3bb2e5c7995d329e637a83cc658c5f3e9f
esed-off <saddledvoma@gmail.com>
2021-09-14 23:40:09 +0300

    Create dz po oaip console.cpp

---
+++ dz po oaip console.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double x, y, z, a, b, c, d, r, h;
+
+	cout << "write x_";	cin >> x;
+	cout << "write y_";	cin >> y;
+	cout << "write z_";	cin >> z;
+	a = exp(fabs(x - y));
+	b = pow(fabs(x - y), (x + y));
+	c = atan(x) + atan(z);
+	d = pow(log(y), 2) + pow(x, 6);
+	h = pow(d, 1 / 3.);
+	r = a * b / c + d;
+	cout << r;
+
+
+}
+
